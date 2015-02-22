3e678f1c30bb55797d0d86d7ed3364712782ca41
Luke Olney <lukeolney@gmail.com>
2015-02-21 21:22:00 -0600

    typo

--- recipetransform/nlp/parse_ingredient.py
+++ recipetransform/nlp/parse_ingredient.py
@@ -1,4 +1,4 @@
-""" Break down ingredients as specified in API below 
+""" Break down ingredients as specified in inferface below 
 
 Quantity and descriptor should be easy - these are contained in the quantity_string. 
 The rest will be a bit harder, but we can probably use pos and syntactic parsing:
