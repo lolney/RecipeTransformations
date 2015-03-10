e18bc14d5fb6ddde98a2fdfcd6ecd22f10115d51
kfunch <kristinfunch2016@u.northwestern.edu>
2015-03-10 18:01:02 -0500

    Update and rename KristinMessyInstructionParsing.py to KristinInstructionParsing.py

--- recipetransform/nlp/KristinInstructionParsing.py
+++ recipetransform/nlp/KristinInstructionParsing.py
@@ -1,79 +1,12 @@
-from BeautifulSoup import BeautifulSoup
-#from recipetransform.nlp.parse_instruction import parse_instruction
-#from recipetransform.nlp.parse_ingredient import parse_ingredient
-import parse_ingredient
-import parse_instruction
-import urllib2
-import nltk
+
+import parsing
 import sys
-
-def parse_raw(url):
-
-	resp = urllib2.urlopen(url)
-	html = resp.read();
-	parsed_html = BeautifulSoup(html)
-
-	return parsed_html
-
-def getIngredients(html):
-	content = html.body.find(id='zoneIngredients')
-
-	ingredient_lis = content.findAll(id='liIngredient')
-
-	ingredients = []
-	for ingredient in ingredient_lis:
-		try:
-			ingredient_amount = ingredient.find('span', attrs={'class':'ingredient-amount'}).text
-			ingredient_name = ingredient.find('span', attrs={'class':'ingredient-name'}).text
-			parsed_ingred = {"q": ingredient_amount, "n": ingredient_name}
-			#parsed_ingred = parse_ingredient(ingredient_amount, ingredient_name)
-			ingredients.append(parsed_ingred)
-		except AttributeError: # expected, because some table elements contain no ingredients
-			pass
-
-	return ingredients
+import nltk
 
 
-def getInstructions(html):
-	content = html.body.find('div', attrs={'class':'directions'})
-
-	instruction_lis = content.findAll('li')
-
-	instructions = []
-	for instruction in instruction_lis:
-		instruction_text = instruction.find('span').text;
-		#parsed_ingred = parse_instruction(instruction_text)
-		instructions.append(instruction_text)
-
-	return instructions
-
-
-def getName(html):
-	return html.body.find(id="itemTitle").text
-
-
-def parseHtml(url):
-
-	parsed_html = parse_raw(url)
-
-	name = getName(parsed_html)
-	ingredients = getIngredients(parsed_html)
-	instructions = getInstructions(parsed_html)
-
-	return {"name" : name, "ingredients" : ingredients, "instructions" : instructions}
-
-
-def main():
-	html = str(sys.argv[1])
-	#instruction =  getInstructions(parse_raw("http://allrecipes.com/Recipe/Carrot-Cupcakes-with-White-Chocolate-Cream-Cheese-Icing/Detail.aspx?soid=home_pins_3"))
-	#instruction =  getInstructions(parse_raw("http://allrecipes.com/Recipe/Sausage-and-Shrimp-Gumbo/Detail.aspx?soid=carousel_0_rotd&prop24=rotd"))
-	#instruction =  getInstructions(parse_raw("http://allrecipes.com/Recipe/Camp-Cornbread/Detail.aspx?evt19=1&referringHubId=342"))
-	#instruction =  getInstructions(parse_raw("http://allrecipes.com/Recipe/Worlds-Best-Lasagna/Detail.aspx?evt19=1&referringHubId=80"))
-	#instruction = getInstructions(parse_raw("http://allrecipes.com/Recipe/Slow-Cooker-Carolina-BBQ/Detail.aspx?evt19=1&referringHubId=17678"))
-	#name = getName(parse_raw("http://allrecipes.com/Recipe/Slow-Cooker-Carolina-BBQ/Detail.aspx?evt19=1&referringHubId=17678"))
+def parse_instruction(list_of_str, name):
 	
-	instruction = getInstructions(parse_raw(html))
-	name = getName(parse_raw(html))
+	instruction = list_of_str
 	
 	name = nltk.word_tokenize(name)
 
@@ -110,7 +43,8 @@ def main():
 			firstWord.append(str1.lower())
 		else:
 			firstWord.append(words[0].lower())
-	print("All methods:\n" + str(firstWord))
+	firstWord1 = list(set(firstWord))
+	#print("\nAll methods:\n" + str(firstWord1))
 
 	cookingMethods = ["bake", "baked", "barbecue", "barbecued", "boil", "boiled", "braise", "braised", "broil", "broiled", "fry", "fried" "grill", "grilled" "microwave", "microwaved", "poach", "poached", "roast", "roasted", "saute", "sauted", "smoke", "smoked", "steam", "steamed"]
 	primaryMethod = "none"
@@ -150,7 +84,6 @@ def main():
 	tools = []
 	i = 0
 	j = 0
-	#toolDict = dict()
 	for instr in instruction:
 		for item in nltk.word_tokenize(instr):
 			if item.lower() == "baking":
@@ -166,27 +99,33 @@ def main():
 				primaryMethod = "slow cooker"
 			if item.lower() in cookingTools:
 				tools.append(item.lower())
-				#toolDict[item.lower()] = 1
 			elif item.lower() in toolTransDict.keys():
 				tools.append(toolTransDict[item.lower()])
 			i = i + 1
 			j = j + 1
 
-	print("Primary method:\n" + str(primaryMethod))
-	#for x in toolDict.keys():
-	#	tools.append(x)
+
+	if primaryMethod == "none" and "cook" in firstWord:
+		primaryMethod = "cook"
+
+	#print("Primary method:\n" + str(primaryMethod))
 	tools = list(set(tools))
-	print("Tools:\n" + str(tools))
+	#print("Tools:\n" + str(tools))
 
 
 	#STEPS
 
 	foodDict = dict()
-	foodDict["bell pepper"] = [["1", "fresh red", ["chopped"], ["finely"]]]
-	timeWords = ["minutes", "seconds", "minute", "second", "hour", "hours"]
+	foodDict["peppers"] = [["2", "green bell", ["sliced"], []], ["1", "red bell", ["sliced"], []]]
+	foodDict["onion"] = [["1", "", ["sliced"], ["thinly"]]]
+	foodDict["mix"] = [["1", "package dry Italian-style salad dressing", [], []]]
+	foodDict["mushrooms"] = [["1 cup", "fresh", ["sliced"], []]]
 	foodDict["potatoes"] = [["1 ounce", "cheesy", ["sliced"], []]]
-	for key in foodDict.keys():
-		print(nltk.word_tokenize(key))
+
+	timeWords = ["minutes", "seconds", "minute", "second", "hour", "hours"]
+	
+	#for key in foodDict.keys():
+	#	print(nltk.word_tokenize(key))
 
 	stepList = []
 	i = 0
@@ -194,7 +133,6 @@ def main():
 	k = 0
 	l = 0
 	for instr in temp2:
-		#words = nltk.word_tokenize(instr)
 		method = firstWord[k]
 		k = k + 1
 		stepTools = []
@@ -241,20 +179,40 @@ def main():
 			for key in foodDict.keys():
 				if item.lower() in nltk.word_tokenize(key):
 					if len(foodDict[key]) > 1:
-						for item in foodDict[key]:
-							if nltk.word_tokenize(instr)[index-1] in nltk.word_tokenize(item[1]):
-								stepIngred.append(str(item[1] + " " + key))
+						for item1 in foodDict[key]:
+							if nltk.word_tokenize(instr)[index-1] in nltk.word_tokenize(item1[1]):
+								stepIngred.append(str(item1[1] + " " + key))
+					elif len(nltk.word_tokenize(key)) > 1:
+						if len(nltk.word_tokenize(instr)) > (index + 1) and nltk.word_tokenize(key)[1] == nltk.word_tokenize(instr)[index + 1]:
+							stepIngred.append(key)
 					else:
 						stepIngred.append(key)
 			index = index + 1
-		#stepDict = dict()
 		if timeFlag == 0:
 			stepTime = ""
 		stepList.append([method, stepIngred, stepTools, stepTime])
-	print(stepList)
+	#print("Steps:")
+	#print(stepList)
+
+	instrDict = dict()
+	instrDict["allMethods"] = firstWord1
+	instrDict["primaryMethod"] = primaryMethod
+	instrDict["tools"] = tools
+	instrDict["steps"] = stepList
+
+	print("\nPrimary Method:\n" + instrDict["primaryMethod"])
+	print("All Methods:\n" + str(instrDict["allMethods"]))
+	print("Tools:\n" + str(instrDict["tools"]))
+	print("Steps:\n" + str(instrDict["steps"]))
 		
 
 
+def main():
+	html = str(sys.argv[1])
+	instruction = parsing.getInstructions(parsing.parse_raw(html))
+	name = parsing.getName(parsing.parse_raw(html))
+
+	parse_instruction(instruction, name)
 
 
 
