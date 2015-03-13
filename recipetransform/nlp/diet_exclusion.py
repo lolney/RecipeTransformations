1b93bfedcd051b7b978b931065dac80bf9185d9b
Luke Olney <lukeolney@gmail.com>
2015-03-13 18:26:14 -0500

    updated diet exclusion

--- recipetransform/nlp/diet_exclusion.py
+++ recipetransform/nlp/diet_exclusion.py
@@ -1,12 +1,17 @@
+import recipetransform.tools.database as tools
+
 meats = ["Beef Products", "Poultry Products", "Pork Products", "Sausages and Luncheon Meats"]
 excluded_food_groups = {
 	"Pescetarian" : meats,
-	"Vegetarian" : meats + ["Finfish and Shellfish Products"],
-	"Vegetarian" : meats + ["Finfish and Shellfish Products"],
-	"Vegetarian" : meats + ["Finfish and Shellfish Products"],
+	"Ovo vegetarian" : meats + ["Finfish and Shellfish Products"],
+	"Lacto vegetarian" : meats + ["Finfish and Shellfish Products"],
+	"Lacto-ovo vegetarian" : meats + ["Finfish and Shellfish Products"],
 	"Vegan" :  meats + ["Finfish and Shellfish Products", "Dairy and Egg Products"]
 }
 
 
-for diet in excluded_food_groups:
-	
+db = tools.DBconnect()
+db.exclusion_table.drop()
+for group in excluded_food_groups:
+	line = {"diet" : group, "excluded" : excluded_food_groups[group]}
+	db.exclusion_table.insert(line)
