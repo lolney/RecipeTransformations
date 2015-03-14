7a18a842c1a8552e3d1f37ccaa2be3960b9976bb
JakeSamson <JakeSamson@users.noreply.github.com>
2015-03-13 21:00:56 -0500

    Update parsing.py

--- recipetransform/nlp/parsing.py
+++ recipetransform/nlp/parsing.py
@@ -1,7 +1,8 @@
 from BeautifulSoup import BeautifulSoup
 from recipetransform.nlp.parse_instruction import parse_instruction
-from recipetransform.nlp.parse_ingredient import parse_ingredient
+from recipetransform.nlp.parse_ingredient import parse_ingredient, parseIngredient, convert_ingredient
 import urllib2
+
 
 def parse_raw(url):
 
@@ -11,27 +12,27 @@ def parse_raw(url):
 
 	return parsed_html
 
-
 def getIngredients(html):
 	content = html.body.find(id='zoneIngredients')
 
 	ingredient_lis = content.findAll(id='liIngredient')
-
-	ingredients = []
+	ingDict = dict()
 	for ingredient in ingredient_lis:
 		try:
 			ingredient_amount = ingredient.find('span', attrs={'class':'ingredient-amount'}).text
-			ingredient_name = ingredient.find('span', attrs={'class':'ingredient-name'}).text
-			parsed_ingred = {"quantity": ingredient_amount, "name": ingredient_name}
+			ingParse = parseIngredient(ingredient.find('span', attrs={'class':'ingredient-name'}).text)
+			if ingParse[0] not in ingDict:
+				ingDict[ingParse[0]] = []
+
+			ingDict[ingParse[0]].append([ingredient_amount, ingParse[1],ingParse[2],ingParse[3]])
 			#parsed_ingred = parse_ingredient(ingredient_amount, ingredient_name)
-			ingredients.append(parsed_ingred)
+			
 		except AttributeError: # expected, because some table elements contain no ingredients
 			pass
 
-	return ingredients
-
-
-def getInstructions(html):
+	return ingDict
+	
+def getInstructions(html,ingDict):
 	content = html.body.find('div', attrs={'class':'directions'})
 
 	instruction_lis = content.findAll('li')
@@ -39,10 +40,9 @@ def getInstructions(html):
 	instructions = []
 	for instruction in instruction_lis:
 		instruction_text = instruction.find('span').text;
+		#parsed_ingred = parse_instruction(instruction_text)
 		instructions.append(instruction_text)
 
-	#parsed_instructions = parse_instructions(instructions)
-	# TODO: possibly separate prep instructions, cook instructions in the interface
 	return instructions
 
 
@@ -51,13 +51,14 @@ def getName(html):
 
 
 def parseHtml(url):
-	
 	parsed_html = parse_raw(url)
 
 	name = getName(parsed_html)
-	ingredients = getIngredients(parsed_html)
-	instructions = getInstructions(parsed_html)
+	ingDict = getIngredients(parsed_html)
+	instructions = getInstructions(parsed_html,ingDict)
+	ingredients = []
+	for x in ingDict.keys():
+		for y in ingDict[x]:
+			ingredients.append(convert_ingredient(x,y))
 
-	return {"name" : name,
-	 "ingredients" : ingredients,
-	 "instructions" : instructions}
+	return {"name" : name, "ingredients" : ingredients, "instructions" : instructions}
