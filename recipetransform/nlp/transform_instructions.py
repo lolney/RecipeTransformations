f3abcb5e3976c21a5a6ba1ec1913f9f57dca7c16
JakeSamson <JakeSamson@users.noreply.github.com>
2015-03-17 22:38:42 -0500

    Update transform_instructions.py

--- recipetransform/nlp/transform_instructions.py
+++ recipetransform/nlp/transform_instructions.py
@@ -4,17 +4,18 @@ def updateAllInstructions(instructions, old_ingredients, new_ingredients):
 	new_instructions = []
 	nameIngDict = dict()
 	for i,ing in enumerate(old_ingredients):
-		if ing['name'] not in nameIngDict:
-			nameIngDict[ing['name']] = []
+		namestr = ing['name'].lower()
+		if namestr not in nameIngDict:
+			nameIngDict[namestr] = []
 
 		changed = True
 		new_ing = new_ingredients[i]
-		if ing['name'] == new_ing['name'] and ing['descriptor'] == new_ing['descriptor']:
+		if namestr == new_ing['name'].lower() and ing['descriptor'] == new_ing['descriptor']:
 			changed = False
-		nameIngDict[ing['name']].append([ing['descriptor'], changed, new_ing['name'],new_ing['descriptor']])
+		nameIngDict[namestr].append([ing['descriptor'].lower(), changed, new_ing['name'].lower(),new_ing['descriptor'].lower().strip()])
 
 	for instruction in instructions:
-		tokens = nltk.word_tokenize(instruction)
+		tokens = nltk.word_tokenize(instruction.lower())
 		for name in nameIngDict:
 			temptokens = []
 			ings = nameIngDict[name]
@@ -26,14 +27,14 @@ def updateAllInstructions(instructions, old_ingredients, new_ingredients):
 				descList = []
 				if not choice:
 					if ings[0][1]:
-						descList = [x for x in nltk.word_tokenize(ings[0][0]) if x != ',']
+						descList = [x for x in nltk.word_tokenize(ings[0][0]) if x not in [',','.']]
 						temptokens += repUpToIngPhrase(lastmatch, matchpos, tokens, descList, ings[0][2], ings[0][3])
 				else:
 					besting = None
 					bestlen = 0
 					bestDescList = []
 					for ing in ings:
-						descList = [x for x in nltk.word_tokenize(ing[0]) if x != ',']
+						descList = [x for x in nltk.word_tokenize(ing[0]) if x not in [',','.']]
 						temppos = matchpos-1
 						tempcount = 1
 						while temppos > lastmatch and tokens[temppos] in descList:
@@ -49,7 +50,8 @@ def updateAllInstructions(instructions, old_ingredients, new_ingredients):
 						temptokens += repUpToIngPhrase(lastmatch, matchpos, tokens, bestDescList, besting[2], besting[3])
 				lastmatch = matchpos
 				matchpos = next(matchitr,None)
-			tokens = temptokens
+			if temptokens:
+				tokens = temptokens + tokens[lastmatch:]
 		new_instructions.append(punctFriendlyJoin(tokens))
 	return new_instructions
 
@@ -62,7 +64,11 @@ def repUpToIngPhrase(lastpos, namepos, instList, descList, newName, newDesc):
 	return instList[lastpos+1:temppos+1] + [newName]
 
 def punctFriendlyJoin(lst):
-	return ''.join([' ' + x if x != ',' else x for x in lst])[1:]
+	lst[0] = lst[0].title()
+	for i,v in enumerate(lst):
+		if i != 0 and lst[i-1] == '.':
+			lst[i] = lst[i].title()
+	return ''.join([' ' + x if x not in [',','.'] else x for x in lst])[1:]
 
 def stringFromList(lst):
 	if len(lst) < 2:
